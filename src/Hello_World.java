dd1b5b6044586dc6bb2b05a52db13e4a0f3c679a
daruvurisai <daruvurisai@gmail.com>
2021-05-24 19:48:26 -0500

    VariableDeclaration

--- src/Hello_World.java
+++ src/Hello_World.java
@@ -1,5 +1,7 @@
 public class Hello_World {
     public static void main(String[] args){
+        int myfirstnum=5+10;
         System.out.println("Hello World !");
+        System.out.println(myfirstnum);
     }
 }
